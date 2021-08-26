3f2563915f1182381d0d1ed661a97a651eb5cccb
Andrew L <andrewl@a-personal.lan>
2021-08-26 18:16:51 -0400

    add pinky self

--- keyboards/xd75/keymaps/alysaght/keymap.c
+++ keyboards/xd75/keymaps/alysaght/keymap.c
@@ -34,11 +34,11 @@
  * |  NO  | Left | Down | Right|  NO  |
  * `----------------------------------'
  */
-#define _________________MID_1_____________________         KC_F1,   KC_P7,   KC_P8,   KC_P9,   KC_P0
-#define _________________MID_2_____________________         KC_F2,   KC_P4,   KC_P5,   KC_P6, KC_F6
-#define _________________MID_3_____________________         KC_F3,   KC_P1,   KC_P2,   KC_P3, KC_F7
-#define _________________MID_4_____________________         KC_F4, XXXXXXX,   KC_UP, XXXXXXX, KC_F8
-#define _________________MID_5_____________________         KC_F5, KC_LEFT, KC_DOWN, KC_RGHT, KC_PDOT
+#define _________________MID_1_____________________         KC_F9,   KC_P7,   KC_P8,   KC_P9,   KC_P0
+#define _________________MID_2_____________________         KC_F1,   KC_P4,   KC_P5,   KC_P6, KC_F5
+#define _________________MID_3_____________________         KC_F2,   KC_P1,   KC_P2,   KC_P3, KC_F6
+#define _________________MID_4_____________________         KC_F3,  KC_F10,   KC_UP,  KC_F11, KC_F7
+#define _________________MID_5_____________________         KC_F4, KC_LEFT, KC_DOWN, KC_RGHT, KC_F8
 
 
 #define LAYOUT_ortho_5x15_wrapper(...) LAYOUT_ortho_5x15(__VA_ARGS__)
